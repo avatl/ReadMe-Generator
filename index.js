d1ca52f3464d3b476886f04fb247ca7c29250128
Ava Lorusso <ava.tlorusso@gmail.com>
2020-10-07 16:15:19 -0400

    mardown.js

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkdown = require(".utils/generateMarkdown");
+const generateMarkdown = require("utils/generateMarkdown.js");
 
 // array of questions for user
 const questions = [
     {
         message: "Enter your github username",
-        name: "gitUsername",
+        name: "username",
         type: "input"
     }, {
         message: "Enter your project name",
-        name: "projectTitle",
+        name: "title",
         type: "input"
     }, {
         message: "Give a brief description of your project",
-        name: "gitRepository",
+        name: "description",
         type: "input"
     }, {
         message: "Did anyone else contribute to this project?",
-        name: "projectContributors",
+        name: "contributors",
         type: "input"
     }, {
         message: "What are the installation instructions for your project?",
-        name: "projectInstructions",
+        name: "instructions",
         type: "input"
     }, {
         message: "What licenses are being used?",
-        name: "projectLicense",
+        name: "license",
         choices: ["MIT", "Apache-2.0", "ISC", "LGPL", "GPL"],
         type: "checkbox"
     }, {
         message: "Describe the usage for this project",
-        name: "projectUsage",
+        name: "usage",
         type: "input"
     }, {
         message: "Have you put your project through any tests? If so, which tests and did they pass?",
-        name: "projectTests",
+        name: "tests",
         type: "input"
     }
 ];
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,43 @@
 // function to generate markdown for README
-function generateMarkdown(userAnswers, data) {
-    return `
-    ${userAnswers.gitUsername}
+function generateMarkdown(data) {
+    return (`# ${data.title}
   
-  `;
+    # Welcome to ${data.title}: 
+
+    ## Description
+      ${data.description}
+
+    ## Table of Contents
+    Navigate through the README Using the Table of Contents : 
+      * [Installation](#installation)
+      * [Usage](#usage)
+      * [License](#license)
+      * [Contributions](#contributing)
+      * [Badges](#badges)
+      * [Tests](#tests)
+      * [Questions](#questions)
+      
+    ## Installation
+      ${data.instructions}
+    
+    ## Usage
+      ${data.usage}
+    
+    ## License
+      ${data.license}
+   
+    ## Contributing
+      ${data.contributors}
+   
+    ## Badges
+    
+    ## Tests
+      ${data.tests}
+    
+    ## Questions
+
+    [Link to Creator Github](https://github.com/${data.username})
+    `)
   }
   
   module.exports = generateMarkdown;
